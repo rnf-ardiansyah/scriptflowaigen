81e062dd081c37cb96c0833d185f248d9ddfc576
Rifqi Nur Fauzi Ardiansyah <rifqi153syah@gmail.com>
2026-07-05 11:33:10 +0700

    feat: generate route tree and update vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,24 @@
 import { defineConfig } from "vite";
+import { nitro } from "nitro/vite";
+import { tanstackStart } from "@tanstack/react-start/plugin/vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
-import { tanstackRouter } from "@tanstack/router-plugin/vite";
 
 export default defineConfig({
-  plugins: [tanstackRouter(), react(), tailwindcss()],
+  plugins: [
+    tanstackStart(), // wajib ada, dan harus SEBELUM react()
+    react(),
+    tailwindcss(),
+    nitro(), // karena "server.ts" custom kamu jalan di atas Nitro
+  ],
   resolve: {
     tsconfigPaths: true,
   },
+  environments: {
+    ssr: {
+      build: {
+        rollupOptions: { input: "./src/server.ts" },
+      },
+    },
+  },
 });
-
