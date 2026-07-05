4a5b2e819a423ef2fe7193501bddf04b27c30411
Rifqi Nur Fauzi Ardiansyah <rifqi153syah@gmail.com>
2026-07-05 11:10:56 +0700

    refactor: Update tanstackStart to tanstackRouter in vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,8 +2,8 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 import tsconfigPaths from "vite-tsconfig-paths";
-import { tanstackStart } from "@tanstack/router-plugin/vite";
+import { tanstackRouter } from "@tanstack/router-plugin/vite";
 
 export default defineConfig({
-  plugins: [tanstackStart(), react(), tailwindcss(), tsconfigPaths()],
+  plugins: [tanstackRouter(), react(), tailwindcss(), tsconfigPaths()],
 });
