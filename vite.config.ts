09194e0a2f7bfd2073673c2545e0417915bae3f8
Rifqi Nur Fauzi Ardiansyah <rifqi153syah@gmail.com>
2026-07-05 11:13:47 +0700

    refactor: remove vite-tsconfig-paths plugin and enable tsconfigPaths in resolve

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,12 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
-import tsconfigPaths from "vite-tsconfig-paths";
 import { tanstackRouter } from "@tanstack/router-plugin/vite";
 
 export default defineConfig({
-  plugins: [tanstackRouter(), react(), tailwindcss(), tsconfigPaths()],
+  plugins: [tanstackRouter(), react(), tailwindcss()],
+  resolve: {
+    tsconfigPaths: true,
+  },
 });
+
